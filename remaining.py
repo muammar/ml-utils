d3d92016d6e0a2bcc5ea1c0dc0a4dc8305fcf3fe
Muammar El Khatib <muammarelkhatib@gmail.com>
2017-02-08 22:39:01 -0500

    Check how many remaining predictions with respect to successful trainings in Amp.

---
+++ remaining.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import glob
+import shutil
+import os
+import re
+
+listing = glob.glob(str(sys.argv[1])+'/*.amp')
+listing = [ element for element in listing if not 'initial' in element ]
+
+print('Number of Amp successful trainings: ',len(listing))
+
+done = 0
+remaining = 0
+
+for element in listing:
+    fname = re.sub(str(sys.argv[1]), '', element).strip('/')[:-4] + '.traj'
+    if os.path.isfile(fname) == True:
+        done += 1
+    else:
+        remaining += 1
+
+print('Predictions done: ', done)
+print('Remaining calculations, ', remaining)
