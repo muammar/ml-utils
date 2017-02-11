883d09e205433438f3918f1ca2fa31dc2dea40ed
Muammar El Khatib <muammarelkhatib@gmail.com>
2017-02-11 15:01:45 -0500

    Adding functions to do string clustering (I hope).

--- set.py
+++ set.py
@@ -3,8 +3,13 @@
 #
 import sys
 import glob
+from fuzzywuzzy import fuzz
+import itertools
 
 def trained_calculations(path):
+    """
+    This function returns a list of successfully trained Amp calculations.
+    """
     listing = glob.glob(str(path)+'/*.amp')
     listing = [ element for element in listing if not 'initial' in element ]
     failed = [ element for element in listing if 'untrained' in element ]
@@ -12,9 +17,26 @@ def trained_calculations(path):
     return list(trained)
 
 def common_calculations(list1, list2):
+    """
+    Return common set of elements in lists.
+    """
     common = list(set(list1).intersection(list2))
     return list(common)
 
 def listing_calculations(path, extension):
+    """
+    List files inside path with extension=extension.
+    """
     listing = glob.glob(path+'/*.'+extension)
     return list(listing)
+
+def compare_strings(string1, string2):
+    """
+    Compare similarity between strings. Useful for doing some clustering work.
+    """
+    similarity = fuzz.ratio(str(string1), str(string2))
+    return similarity
+
+def pair_iteration(listofelements):
+    for pair in itertools.product(listofelements, repeat=2):
+        return pair
