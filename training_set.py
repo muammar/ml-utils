db9b8e76273f9f1b4b0d723f7499acd07caf11d4
Muammar El Khatib <muammarelkhatib@gmail.com>
2017-11-30 16:02:04 -0500

    New file added for partitioning training set.

---
+++ training_set.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from ase.io import Trajectory
+import random
+
+
+def split(images, trainingname='trainingimages.traj',
+        testname='testimages.traj', shuffle=True, test_set=20):
+    """docstring for parition_training_set
+
+
+    test_set : integer
+        Porcentage of training data that will be used as test set.
+    """
+    images = Trajectory(images)
+
+    total_length = len(images)
+    test_length = (test_set * total_length / 100)
+    training_leght = total_length - test_length
+
+    _images = range(len(images))
+
+    if shuffle is True:
+        random.shuffle(_images)
+
+    trainingimages = []
+    ti = Trajectory(trainingname, mode='w')
+
+    log = open('log.txt', 'w')
+
+    for i in _images[0:training_leght]:
+        trainingimages.append(i)
+        ti.write(images[i])
+    log.write(str(trainingimages))
+    log.write('\n')
+
+    testimages =[]
+    test = Trajectory(testname, mode='w')
+    for i in _images[-test_length:-1]:
+        testimages.append(i)
+        test.write(images[i])
+
+    log.write(str(testimages))
+    log.close()
+    return
